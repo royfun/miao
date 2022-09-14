afb49c047ff29f98e5a834d8efb4f4fdce828403
luoyifan <736325108@qq.com>
2022-09-14 20:31:17 +0800

    3

--- lodash/royfun-lodash.js
+++ lodash/royfun-lodash.js
@@ -1,6 +1,6 @@
 var royfun = function () {
 
-  function chunk (array, size = 1) {
+  function chunk(array, size = 1) {
     let result = []
     for (let i = 0; i < array.length;) {
       let group = []
@@ -16,10 +16,34 @@ var royfun = function () {
     return result
   }
 
+  function compact(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
+  function concat(array, ...values) {
+    let res = slice(array)
+    values.forEach(it => {
+      if (isArray(it)) {
+        res.push(...it)
+      } else {
+        res.push(it)
+      }
+    })
+  }
 
 
   return {
-    chunk
+    chunk,
+    compact,
+    concat
+
+
   }
 
-}()
+} ()
