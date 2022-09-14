47af32208d8f2512fd740fb5c7a287765431494e
luoyifan <736325108@qq.com>
2022-09-14 18:06:43 +0800

    09141806

--- lodash/royfun-lodash.js
+++ lodash/royfun-lodash.js
@@ -1,15 +1,21 @@
-function chunk(array, size = 1) {
-  let result = []
-  for (let i = 0; i < array.length; ) {
-    let group = []
-    for (let j = 0; j < size; j++) {
-      group.push(array[i])
-      i++
-      if (i == array.length) {
-        break
+var royfun = function () {
+
+  function chunk(array, size = 1) {
+    let result = []
+    for (let i = 0; i < array.length;) {
+      let group = []
+      for (let j = 0; j < size; j++) {
+        group.push(array[i])
+        i++
+        if (i == array.length) {
+          break
+        }
       }
+      result.push(group)
     }
-    result.push(group)
+    return result
   }
-  return result
+
+
+
 }
