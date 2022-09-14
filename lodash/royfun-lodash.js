32eb138cf634b9d71e72aa0baacee95545dc8015
luoyifan <736325108@qq.com>
2022-09-14 17:51:54 +0800

    chunk

--- lodash/royfun-lodash.js
+++ lodash/royfun-lodash.js
@@ -1,5 +1,15 @@
-var pao = {
-  pp() {
-    console.log(this)
+function chunk(array, size = 1) {
+  let result = []
+  for (let i = 0; i < array.length; ) {
+    let group = []
+    for (let j = 0; j < size; j++) {
+      group.push(array[i])
+      i++
+      if (i == array.length) {
+        break
+      }
+    }
+    result.push(group)
   }
+  return result
 }
